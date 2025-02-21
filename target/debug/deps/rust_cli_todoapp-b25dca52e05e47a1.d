1121bb04a5c46c03735a5ee98b23da23d5a72082
anjali gupta <sajalanjali3@gmail.com>
2025-02-21 12:57:20 +0530

    first commit

---
+++ target/debug/deps/rust_cli_todoapp-b25dca52e05e47a1.d
@@ -0,0 +1,5 @@
+/home/anjali/rust-cli-todoapp/target/debug/deps/rust_cli_todoapp-b25dca52e05e47a1: src/main.rs
+
+/home/anjali/rust-cli-todoapp/target/debug/deps/rust_cli_todoapp-b25dca52e05e47a1.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rust-cli-todoapp.d
@@ -0,0 +1,1 @@
+/home/anjali/rust-cli-todoapp/target/debug/rust-cli-todoapp: /home/anjali/rust-cli-todoapp/src/main.rs
